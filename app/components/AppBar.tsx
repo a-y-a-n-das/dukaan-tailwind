fb0eab8efc04ab601eea2531394083c4479494f8
Ayan Das <126937849+a-y-a-n-das@users.noreply.github.com>
2026-01-23 13:13:25 +0530

    Update AppBar.tsx

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -9,7 +9,7 @@ function AppBar() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="size-8"
+            className="size-8"
           >
             <path
               stroke-linecap="round"
